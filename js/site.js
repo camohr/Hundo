b89029c4aafb1436dac0e7cc5f20c760ddb3bc5e
camohr <16599134+camohr@users.noreply.github.com>
2022-04-18 23:37:29 -0700

    Did it my own way.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,47 @@
-// Call Hello World
-function HelloWorld() {
-    alert("hello there");
+// controller
+function getValues() {
+
+    let sv = parseInt (document.getElementById ("startValue").value);
+    let ev = parseInt (document.getElementById("endValue").value);
+
+    if (!Number.isInteger(sv) || !Number.isInteger(ev)) {
+        alert ("Please enter a number only.")
+        return;
+    }
+
+    let numbers = generateNumbers (sv, ev);
+    displayNumbers (numbers);
+}
+
+// logic
+function generateNumbers (start, end) {
+    if (end < start)  // ensure start less than end
+    {
+        let m = end;
+        end = start;
+        start = m;
+    }
+    let numbers = [];
+    for (let i = start; i <= end; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+// display
+function displayNumbers(numbers) {
+
+    let trows = "";
+
+    for (let i = 0; i<numbers.length; i++) {
+        let n = numbers[i];
+        let className = "";
+        if (n % 2)
+            className = "odd";
+        else
+            className = "even";
+        trows += `<tr><td class="${className}">${n}</td></tr>\n`;
+    }
+    document.getElementById ("resbody").innerHTML = trows;
 }
 
